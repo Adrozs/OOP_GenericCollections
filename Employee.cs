7b5b8dd047448689336789e9eb06cc8ab0d2aaa0
Adrozs <adrozsahegyi@gmail.com>
2023-10-05 13:21:01 +0200

    Moved Employee class to its own file

---
+++ Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    // Employee class
+    public class Employee
+    {
+        // Declare class variables
+        public string Id;
+        public string Name;
+        public string Gender;
+        public string Salary;
+
+        // Assigns input to class' variables
+        public Employee(string id, string name, string gender, string salary)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender;
+            Salary = salary;
+        }
+
+        // Overrides ToString method to provide custom string representation 
+        public override string ToString()
+        {
+            return $"Id: {Id} | Name: {Name} | Gender: {Gender} | Salary: {Salary} |";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -115,30 +115,4 @@ namespace GenericCollections
 
         }
     }
-
-
-    // Employee class
-    public class Employee
-    {
-        // Declare class variables
-        public string Id;
-        public string Name;
-        public string Gender;
-        public string Salary;
-
-        // Assigns input to class' variables
-        public Employee(string id, string name, string gender, string salary)
-        {
-            Id = id;
-            Name = name;   
-            Gender = gender;
-            Salary = salary;
-        }
-
-        // Overrides ToString method to provide custom string representation 
-        public override string ToString()
-        {
-            return $"Id: {Id} | Name: {Name} | Gender: {Gender} | Salary: {Salary} |";
-        }
-    }
 }
